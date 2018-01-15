e6df442d805b42e97e8a7e89de79bdc46c124fa2
Duncan Garmonsway <nacnudus@gmail.com>
2018-01-15 17:25:46 +0000

    Run on all TFL series to filter for interesting changes

---
+++ R/bulk.R
@@ -0,0 +1,125 @@
+# Apply a method to a whole corpus of timeseries, namely the London Underground
+# performance data.
+
+# This was an attempt to maintain both a state and a log, updating both with
+# each new observation.
+
+# That proved to be a pain, and not necessary to demonstrate what a log would
+# look like.  Another implementation that doesn't attempt incremental updates is
+# in R/one-off.R
+
+library(tidyverse)
+library(underground)
+library(cpm)
+library(here)
+
+series <-
+  underground %>%
+  filter(!is.na(fourweek), !is.na(line)) %>%
+  mutate(period = as.numeric(str_sub(year, 1L, 4L)) + fourweek / 13) %>%
+  select(metric, line, period, value) %>%
+  arrange(period) %>%
+  nest(-metric, -line)
+
+stat <- "Mood"
+ARL0 <- 100
+startup <- 20
+
+# Warm up the system by applying onstructing the CPM objects on the startup
+# number of observations.
+
+blank <- makeChangePointModel(cpmType = stat, ARL0 = ARL0, startup = startup)
+blanks <- mutate(series,
+                 cpm = list(blank),
+                 detection_times = list(integer()),
+                 changepoints = list(integer()))
+
+update_with_new_value <- function(.data, cpm, observation) {
+  new_value <- slice(.data, observation)$value
+  processObservation(cpm, new_value)
+}
+
+observations <- blanks
+observation <- 0L
+for (i in seq_len(startup - 1)) {
+  observation <- observation + 1L
+  observations <-
+    observations %>%
+    mutate(cpm = pmap(list(data, cpm, observation), update_with_new_value),
+           detected = map_lgl(cpm, changeDetected),
+           detection_times = map2(detected,
+                                  detection_times,
+                                  ~ if(.x) c(.y, observation) else .y),
+           changepoints = pmap(list(detected, changepoints, cpm),
+                               ~ if(..1) c(..2, which.max(getStatistics(..3))) else ..2)) %>%
+    select(-detected)
+}
+print(observation)
+observations %>%
+  filter(detected)
+
+# Make a further observation and update the model
+observation <- observation + 1L
+observations <-
+  observations %>%
+    mutate(cpm = pmap(list(data, cpm, observation), update_with_new_value),
+           detected = map_lgl(cpm, changeDetected),
+           detection_times = map2(detected,
+                                  detection_times,
+                                  ~ if(.x) c(.y, observation) else .y),
+           changepoints = pmap(list(detected, changepoints, cpm),
+                               ~ if(..1) c(..2, which.max(getStatistics(..3))) else ..2)) %>%
+    select(-detected)
+print(observation)
+
+observations %>%
+  filter(detected)
+
+observations %>%
+  filter(detected) %>%
+  pull(detection_times)
+
+observations %>%
+  filter(detected) %>%
+  pull(changepoints)
+
+plot_changepoints <- function(metric,
+                              line,
+                              data,
+                              detection_times,
+                              changepoints) {
+  plotdata <- slice(data, seq_len(observation))
+  changepoints <- tibble(changepoint = plotdata$period[changepoints],
+                         detection_time = plotdata$period[detection_times],
+                         y = plotdata$value[changepoints])
+  ggplot(plotdata, aes(period, value)) +
+    geom_line(colour = "grey80") +
+    geom_vline(aes(xintercept = changepoint),
+                 colour = "red",
+                 data = changepoints,
+                 linetype = 5) +
+    geom_point(aes(x = changepoint, y = y), data = changepoints, colour = "black") +
+    geom_segment(aes(x = changepoint, xend = detection_time, y = y, yend = y),
+               data = changepoints,
+               colour = "black") +
+    theme_void()
+}
+
+plotseries <-
+  observations %>%
+  filter(detected) %>%
+  slice(1)
+
+plot_changepoints(plotseries$metric,
+                  plotseries$line,
+                  plotseries$data[[1]],
+                  plotseries$detection_times[[1]],
+                  plotseries$changepoints[[1]])
+
+metric <- plotseries$metric
+line <- plotseries$line
+data <- plotseries$data[[1]]
+detection_times <- plotseries$detection_times[[1]]
+changepoints <- plotseries$changepoints[[1]]
+
+getStatistics(x)
---
+++ R/one-off.R
@@ -0,0 +1,114 @@
+# Apply a method to a whole corpus of timeseries, namely the London Underground
+# performance data.
+
+# This doesn't attempt to update the model or the log with new entries.  It
+# reads all entries in one go, and prints plots of the most-recently-detected
+# changepoints
+
+# An attempt to update the model and the log is in R/one-off.R
+
+library(tidyverse)
+library(underground)
+library(cpm)
+library(here)
+
+stat <- "Mood"
+ARL0 <- 100
+startup <- 20
+
+fourweek_to_period <- function(year, fourweek) {
+  as.integer(str_sub(year, 1L, 4L)) + 0.25 + as.integer(fourweek) / 13
+}
+
+fourweek_to_text <- function(year, fourweek) {
+  paste0(year, "/", sprintf("%02i", fourweek))
+}
+
+text_to_period <- function(text) {
+  as.integer(str_sub(text, 1L, 4L)) + as.integer(str_sub(text, -2L)) / 13
+}
+
+# Detect all changepoints in all series
+series <-
+  underground %>%
+  filter(!is.na(fourweek), !is.na(line)) %>%
+  mutate(period = fourweek_to_period(year, fourweek)) %>%
+  select(metric, line, period, value) %>%
+  arrange(period) %>%
+  nest(-metric, -line) %>%
+  mutate(model = map(data, ~ processStream(.x$value, stat, ARL0, startup))) %>%
+  mutate(changePoints = map2(data, model, ~ .x$period[.y$changePoints + 1]),
+         detectionTimes = map2(data, model, ~ .x$period[.y$detectionTimes]),
+         changeValues = map2(data, model, ~ .x$value[.y$changePoints + 1]))
+
+# Extract only the changepoints, as a log
+changes <-
+  series %>%
+  select(-data, -model) %>%
+  unnest()
+
+# Print the log, ordered by the detection date, which is the order they would be
+# reported
+changes %>%
+  arrange(detectionTimes, changePoints, metric, line) %>%
+  print(n = Inf)
+
+plot_series <- function(metric, line, changePoints, detectionTimes,
+                        changeValues, ...) {
+  changepoints <- tibble(changePoints, detectionTimes, changeValues)
+    # tibble(changePoints = text_to_period(changePoints),
+    #        detectionTimes = text_to_period(detectionTimes),
+    #        changeValues)
+  .metric <- metric
+  .line <- line
+  series %>%
+    filter(metric == .metric, line == .line) %>%
+    select(metric, line, data) %>%
+    unnest() %>%
+    # mutate(period = text_to_period(period)) %>%
+    ggplot(aes(period, value)) +
+    geom_line(colour = "grey80") +
+    geom_vline(aes(xintercept = changePoints),
+                 colour = "red",
+                 data = changepoints,
+                 linetype = 5) +
+    geom_point(aes(x = changePoints, y = changeValues), data = changepoints, colour = "black") +
+    geom_segment(aes(x = changePoints, xend = detectionTimes,
+                     y = changeValues, yend = changeValues),
+               data = changepoints,
+               colour = "black") +
+    theme_void()
+}
+
+# Plot the latest detections
+latest_changes <- filter(changes, detectionTimes == max(detectionTimes))
+plots <-
+  latest_changes %>%
+  select(metric, line) %>%
+  inner_join(series) %>%
+  pmap(plot_series)
+for (i in seq_along(plots)) {
+  cat(i, "\n")
+  ggsave(glue("{i}.png"), plots[[i]])
+}
+
+# Plot the nth-latest detections
+# https://stackoverflow.com/a/4916318/937932
+x <- -changes$detectionTimes
+x_unique <- unique(x)
+x_ranks <- rank(x_unique)
+changes$order <- as.integer(x_ranks[match(x,x_unique)])
+ordered_changes <-
+  changes %>%
+  arrange(order, changePoints, metric, line)
+ordered_changes
+plots <-
+  ordered_changes %>%
+  filter(order == 9) %>%
+  select(metric, line) %>%
+  inner_join(series) %>%
+  pmap(plot_series)
+for (i in seq_along(plots)) {
+  cat(i, "\n")
+  ggsave(glue("{i}.png"), plots[[i]])
+}
